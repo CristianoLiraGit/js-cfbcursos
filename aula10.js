a3827f8e8c00bd710c3c6123a239b3038617eb59
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-25 01:29:54 -0300

    Update aula10.js

--- aula10.js
+++ aula10.js
@@ -1,3 +1,5 @@
+// Operador ternário
+
 // let n = 11  // É uma forma de reduzir uma operação condicional
 // let res = n % 2
 
@@ -18,4 +20,15 @@ console.log(res)
 
 let st = 'I'
 res=(st == 'A' ? 'Ativo' : 'Inativo')
+console.log(res)
+
+let num = 10
+res=(!(num%2) ? 'Par' : 'Ímpar')
+console.log(res)
+
+// 0 = False
+// 1 = True - por isso neguei "!"
+
+let sit = 'A'
+res = (sit == 'A' ? 'Ativo' : 'Inativo')
 console.log(res)
