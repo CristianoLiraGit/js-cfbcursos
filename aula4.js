f1e7d1db2647a4f4cc081de483f4ad5efa8a0729
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-24 23:29:50 -0300

    Update aula4.js

--- aula4.js
+++ aula4.js
@@ -1,8 +1,18 @@
-let num1=0, num2=0, res=0
+// Operadores Aritméticos
+
+let num1=0, num2=0, res=0 // As variáveis podem ser declaradas separadamente também.
 
 num1 = 10
 
 num2 ++
 
+// num1 --
+
 console.log(num1, num2)
 console.log(`O primeiro número é ${num1} e o segundo é ${num2}`)
+
+// Ordem de precedência
+
+/*
+
+*/
