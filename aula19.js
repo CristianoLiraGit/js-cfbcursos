d13cf295d4a90f0531eefc7842b682b22374963a
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-26 02:04:54 -0300

    Update aula19.js

--- aula19.js
+++ aula19.js
@@ -1,18 +1,24 @@
+//      ***    Comando break    ***
+// Geralmente dentro de uma condicional. Serve para para o loop.
+
 // let n = 0
 // let max = 1000
 
 // while(n<max){
 //     console.log(`CFB Cursos - ${n}`)
 //     if (n > 10){
-//         break
+//         break // este comando para a execução do programa
 //     }
 //     n++
 // }
 
 // console.log('Fim do programa')
 
+//      ***    Comando continue     ***
+// Para somente determinada iteração, continuando o restante do programa. Exemplo:
+
 // let n = 0
-// let max = 100
+// let max = 1000
 // let pares = 0
 
 // for (let i = n; i < max; i++) {
@@ -23,4 +29,20 @@
 // }
 
 // console.log(`O número de pares é ${pares}. Fim do programa `)
+// console.log('Fim do programa')
+
+//  *** Exemplificando o continue ***
+
+let n = 0
+let max = 1000
+let pares = 0
+
+for(let i=n; i<max; i++){
+    if(i%2 !=0){
+        continue // Para somente esta iteração quando o valor iterado é ímpar...
+    }
+    pares++ // ... e incrementa apenas o número par na próxima iteração.
+}
+
+console.log(`Quantidade de pares: ${pares}.`)
 
