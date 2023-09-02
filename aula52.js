d1988c4074ddfcce2177efd7b082c03e5cbe760c
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-02 00:45:21 -0300

    Update aula 52

---
+++ aula52.js
@@ -0,0 +1,20 @@
+const caixa = document.querySelector('#caixa');
+
+let cores=['azul', 'verde', 'vermelho', ['claro', 'escuro', 'médio']]
+let cursos=["HTML", "CSS", "JavaScript", cores];
+
+// cursos[0]="C++"
+
+cursos.push("C++"); // Adiciona no final da lista
+cursos.push('Python'); // Adiciona no final da lista
+cursos.pop(); // Elimina o último elemento (Python)
+cursos.unshift('Python'); // Adiciona Python no início
+cursos.shift(); // Elimina Python do início
+
+console.log(cursos[3][1][2]); // Conceito de matrizes
+
+cursos.map((el)=>{
+    let p = document.createElement("p");
+    p.innerHTML=el;
+    caixa.appendChild(p)
+})
