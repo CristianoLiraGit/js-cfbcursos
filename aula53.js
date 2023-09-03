2338008664720b605f37f399607397d792bd320e
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-03 12:34:07 -0300

    Update aula53.js

---
+++ aula53.js
@@ -0,0 +1,26 @@
+let valores = [1, 2]
+const op = [
+    (val)=>{
+        let res = 0
+        for(v of val){
+            res+=v
+        }
+        return res
+    },
+    (val)=>{
+        let res= 1
+        for(v of val){
+            res*=v
+        }
+        return res
+    },
+    (val)=>{
+        for(v of val){
+            console.log(v)
+        }
+    }
+]
+// console.log(op[0](valores))
+console.log(`A soma é ${op[0](valores)}.`)
+console.log(`A multiplicação é ${op[1](valores)}.`)
+op[2](valores)
