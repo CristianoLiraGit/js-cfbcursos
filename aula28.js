626e456d38f14aaca949abf21158867fd4e45c01
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-27 22:35:00 -0300

    Update aula28.js

--- aula28.js
+++ aula28.js
@@ -5,6 +5,15 @@
 //     console.log(`Curso: ${el} - Posição: ${i}`)
 // })
 
+/*
+let el = document.getElementsByTagName("div")
+el=[...el]
+console.log(el)
+el.map((e,  i)=>{
+    e.innerHTML="CFB Cursos"
+})
+*/
+
 const converterInt=(e) => parseInt(e)
 const dobrar=(e) => e*2
 let num = ['1', '2', '3', '4', '5'].map(dobrar)
