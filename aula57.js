b111d493b944b68941a8ed0555022fa67723f6bf
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-09-27 23:15:13 -0300

    Create aula57.js
    Aula sobre objetos no JavaScript

---
+++ aula57.js
@@ -0,0 +1,13 @@
+class Pessoa{
+    constructor(nome){
+        this.nome=nome
+    }
+}
+
+let p1 = new Pessoa("Cristiano")
+let p2 = new Pessoa("Natália")
+let p3 = new Pessoa("Gordo")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
