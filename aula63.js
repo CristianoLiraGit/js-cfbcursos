566e302ff537bd5dc611f9565cbe0da8fb003e3e
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-10-02 01:18:00 -0300

    Update aula 63

--- aula63.js
+++ aula63.js
@@ -1,6 +1,52 @@
-// Herança
-// Ocorre quando uma classe filha herda as propriedades de uma classe pai
+const f_tipoMilitar = document.querySelector('#f_tipoMilitar')
+const f_tipoNormal = document.querySelector('#f_tipoNormal')
+const f_blindagem = document.querySelector('#f_blindagem')
+const f_municao = document.querySelector('#f_municao')
+const carros = document.querySelector('#carros')
+const btn_addCarro = document.querySelector('#btn_addCarro')
+const f_nome = document.querySelector('#f_nome')
+const f_portas = document.querySelector('#f_portas')
 
+let a_carros = []
+
+f_tipoMilitar.addEventListener('click', (evt)=>{
+    f_nome.value="" // Apagar os campos
+    f_portas.value=0 // Apagar os campos
+    f_blindagem.value=0
+    f_municao.value=0
+    f_blindagem.removeAttribute('disabled')
+    f_municao.removeAttribute('disabled')
+})
+
+f_tipoNormal.addEventListener('click', (evt)=>{
+    f_nome.value=""  // Apagar os campos
+    f_portas.value=0 // Apagar os campos
+    f_blindagem.value=0
+    f_municao.value=0
+    f_blindagem.setAttribute('disabled', 'disabled')
+    f_municao.setAttribute('disabled', 'disabled')
+})
+
+const gerenciarExibicaoCarros=()=>{
+    carros.innerHTML=""
+    a_carros.forEach((c)=>{
+        const div=document.createElement('div')
+        div.setAttribute('class', 'carro')
+        div.innerHTML=c.nome
+        carros.appendChild(div)
+    })
+}
+
+btn_addCarro.addEventListener('click', ()=>{
+    if(f_tipoNormal.checked){
+        const c = new Carro(f_nome.value, f_portas.value)
+        a_carros.push(c)
+    }else{
+        const c = new Carro(f_nome.value, f_portas.value, f_blindagem.value, f_municao.value)
+        a_carros.push(c)
+    }
+    gerenciarExibicaoCarros()
+})
 class Carro { // Classe pai
     constructor(nome, portas){
         this.nome=nome
@@ -25,6 +71,7 @@ class Militar extends Carro{ // Classe filho. O comando extends faz a classe her
         super(nome, portas) // Invoca algo específico da classe  pai
         this.blindagem=blindagem
         this.municao=municao
+        this.setCor('Verde')
     }
     atirar=function(){
         if(this.municao>0){
@@ -72,3 +119,6 @@ console.log(`Nome: ${c3.nome}.`)
 console.log(`Portas: ${c3.portas}.`)
 
 console.log("-------------------------------------")
+
+
+// Terminar a parte final do código
