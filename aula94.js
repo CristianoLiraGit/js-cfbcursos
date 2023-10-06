4c8f207ef3b75a3d2fc0066ef261c4712cc10d76
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-10-06 01:20:16 -0300

    Update aula94.js

--- aula94.js
+++ aula94.js
@@ -1,8 +1,14 @@
 // import getTodosCursos, {cursos} from './cursos3.js'
 
-import getTodosCursos from "./cursos3.js";
-import { cursos, getCurso } from "./cursos3.js";
+// Renomeando módulos
 
-console.log(cursos)
-console.log(getTodosCursos())
-console.log(getCurso[0])
+import getTodosCursos from "./cursos3.js"; // não usou chaves porque foi exportado como "export defaut"
+// import { cursos as c, getCurso as gc } from "./cursos3.js";
+import * as n_cursos from "./cursos3.js"; // Aqui, o asterisco (*) pega tudo que é exportado no módulo
+
+// console.log(c)
+// console.log(getTodosCursos())
+// console.log(gc[0])
+console.log(n_cursos.cursos)
+console.log(n_cursos.getCurso(1))
+console.log(n_cursos.default)
