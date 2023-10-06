1fa1602ddd31ef642fe0d412e626a9f72f5cb0c1
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-10-06 01:09:45 -0300

    Update aula94.js

---
+++ aula94.js
@@ -0,0 +1,8 @@
+// import getTodosCursos, {cursos} from './cursos3.js'
+
+import getTodosCursos from "./cursos3.js";
+import { cursos, getCurso } from "./cursos3.js";
+
+console.log(cursos)
+console.log(getTodosCursos())
+console.log(getCurso[0])
