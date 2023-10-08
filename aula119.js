0b3f683b0f22451afab893a222198a0b737dae38
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-10-08 10:50:09 -0300

    Create aula119.js

---
+++ aula119.js
@@ -0,0 +1,7 @@
+const f_nome=document.querySelector('#f_nome');
+const f_nota=document.querySelector('#f_nota');
+const f_msg=document.querySelector('#f_msg');
+
+document.querySelector("#btn_validar").addEventListener('click', (evt)=>{
+    evt.preventDefault()
+})
