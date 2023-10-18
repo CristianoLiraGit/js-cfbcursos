4b7bc3b550a0067211e30a592f5078e66f1c09e7
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-10-18 01:36:49 -0300

    Update aula76 javascript cfb cursos

--- aula76.js
+++ aula76.js
@@ -25,31 +25,38 @@ numero.innerHTML='Processando...'
 //! Segunda parte da aula: Aplicando uma promise
 
 const numero=document.getElementById('numero')
+const btn_promessa=document.getElementById('btn_promessa')
 
-let promise=new Promise((deu_certo, deu_errado)=>{ // Coloquei parâmetros com nomes sujestivos/explicativos (pode ser qualquer nome para os parâmetros)
-    let resultado=false //! valor fixo como falso
-    let tempo=3000
-    setTimeout(()=>{
-        if(resultado){
-            deu_certo('Deu tudo certo')
-        }else{
-            deu_errado('Deu tudo errado')
-        }
-    },tempo)
+btn_promessa.addEventListener('click', (evt)=>{
+    numero.innerHTML='Processando...' 
+    promessa() // Chamar a função promessa criada abaixo       
 })
+    
+const promessa=()=>{
+    let promise=new Promise((deu_certo, deu_errado)=>{ // Coloquei parâmetros com nomes sujestivos/explicativos (pode ser qualquer nome para os parâmetros)
+        let resultado=false //! valor fixo como falso
+        let tempo=3000
+        setTimeout(()=>{
+            if(resultado){
+                deu_certo('Deu tudo certo')
+            }else{
+                deu_errado('Deu tudo errado')
+            }
+        },tempo)
+    })
+        promise.then((retorno)=>{  // Se deu certo, cai aqui
+            numero.innerHTML=retorno
+            numero.classList.remove('erro')
+            numero.classList.add('ok')
+        }) 
+        
+        promise.catch((retorno)=>{  // Se deu errado, cai aqui
+            numero.innerHTML=retorno
+            numero.classList.add("erro")
+            numero.classList.remove('ok')
+        }) 
+}
+    numero.innerHTML='Esperando'
 
-promise.then((retorno)=>{  // Se deu certo, cai aqui
-    numero.innerHTML=retorno
-    numero.classList.remove('erro')
-    numero.classList.add('ok')
-}) 
-
-promise.catch((retorno)=>{  // Se deu errado, cai aqui
-    numero.innerHTML=retorno
-    numero.classList.add("erro")
-    numero.classList.remove('ok')
-}) 
-
-numero.innerHTML='Processando...'
 
 // Continuar...
