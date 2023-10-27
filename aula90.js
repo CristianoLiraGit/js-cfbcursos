762cf49e644d96b73fccbca568c606bb0affde97
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-10-27 01:02:16 -0300

    Update aula 90
    Atualizei os arquivos js e css da aula 90.

--- aula90.js
+++ aula90.js
@@ -3,9 +3,44 @@ const objetos=document.getElementById('objetos')
 const computador={ // A chave indica que é um objeto
     cpu: 'i9',
     ram: '64gb', //! Aqui são as propriedades do objeto da classe computador
-    hd: '2tb' 
+    hd: '2tb',
+    info: function(){
+        console.log(`CPU: ${this.cpu}`)
+        console.log(`RAM: ${this.ram}`)
+        console.log(`HD: ${this.hd}`)
+    } 
 }
 
-console.log(computador)
+const computadores = [
+    {
+        cpu:"i9",
+        ram:"64gb",
+        hd:"2tb"
+    },
+    {
+        cpu:"i7",
+        ram:"32gb",
+        hd:"2tb"
+    },
+    {
+        cpu:"i5",
+        ram:"16gb",
+        hd:"1tb"
+    }
+]
 
-objetos.innerHTML=JSON.stringify(computador) // exibir no DOM o objeto computador convertido em JSON (todo separado)
+computadores.forEach((c)=>{
+    const div=document.createElement('div')
+    div.innerHTML=c.cpu+"<br/>"+c.ram+"<br/>"+c.hd
+    div.setAttribute('class', 'computador')
+    objetos.appendChild(div)
+})
+
+/*
+computador.info()
+console.log(computador)
+console.log(computadores)
+*/
+//objetos.innerHTML=JSON.stringify(computador) // exibir no DOM o objeto computador convertido em JSON (todo separado)
+
+objetos.innerHTML=JSON.stringify(computadores)
