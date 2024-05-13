ac68003aa9d89219250fd92360ed9948004314f7
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2024-05-12 21:26:11 -0300

    Update aula24.js

--- aula24.js
+++ aula24.js
@@ -17,9 +17,9 @@ console.log(f(10, 5 ))
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-// Função contrutor
+//! Função contrutor
 
-const f = new Function("v1", "v2", "v3", "return v1 + v2 + v3")
+const g = new Function("v1", "v2", "v3", "return v1 + v2 + v3")
 
         console.log(f(10, 5, 15))
 
