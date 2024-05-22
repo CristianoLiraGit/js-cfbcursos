5f3424b614fd085b0c3c2ee0e59890f13cc25ade
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2024-05-21 22:28:08 -0300

    Update aula 32
    Fiz uma função para adicionar uma classe com o clique de um botão: coloquei uma função dentro de outra função. hehehe

--- aula32.js
+++ aula32.js
@@ -3,11 +3,16 @@ const cursosTodos=[...document.getElementsByClassName('curso')]
 const cursosc1=[...document.getElementsByClassName('c1')]
 const cursosc2=[...document.getElementsByClassName('c2')]
 
+btn = document.getElementById('btn')
 
 console.log(cursosTodos)
 console.log(cursosc1)
 console.log(cursosc2)
 
-cursosc2.map((el) => { /* Função para pegar elemento por elemento e adicionar a classe "destaque"*/
-    el.classList.add('destaque')
+btn.addEventListener('click', ()=>{ // coloquei uma função dentro de outra: 
+    cursosc2.map((el) => { /* Função para pegar elemento por elemento e adicionar a classe "destaque"*/
+        el.classList.add('destaque')
+    })
 })
+
+// Fazer uma função ligada a um botão para limpar a formatação, deixando a classe inicial
